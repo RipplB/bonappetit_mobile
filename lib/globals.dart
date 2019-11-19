ac8cc171554ff963905f58bc1aa44d9cd556e05a
Balazs Rippl <balrip.play@gmail.com>
2019-11-19 14:58:54 +0100

    Main app frame complete

---
+++ lib/globals.dart
@@ -0,0 +1,1 @@
+int activePage = 0;
--- lib/menu.dart
+++ lib/menu.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'globals.dart';
+
+List<String> _title = ["Betöltés...","Rendelés","Lemondás","Korábbi rendelések","Információ","Profil","Kollégák"];
+final _appBarKey = GlobalKey<_PermaAppBarState>();
 
 class Menu extends StatefulWidget{
   State<Menu> createState() => _MenuState();
 }
 class _MenuState extends State<Menu>{
-  String _name = "Betöltés...";
   @override
   Widget build(BuildContext context){
     final String cookie = ModalRoute.of(context).settings.arguments;
-    _getName(cookie, 'https://www.bonappetit.hu/ebed-hazhozszallitas/rendeles');
+    if (_title[0] == "Betöltés...") {
+      _getName(cookie, 'https://www.bonappetit.hu/ebed-hazhozszallitas/rendeles');
+    }
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xFFFFE5BA),
-        appBar: AppBar(
-          backgroundColor: Color(0xFF620000),
-          title: Text(
-            _name,
-            style: TextStyle(
-              color: Color(0xFFFFFFFF),
-              fontSize: 25.0
-            ),
+        appBar: PermaAppBar(
+          key: _appBarKey,
+        ),
+        drawer: Drawer(
+          child: Container(  
+          child: Column(
+            children: createDrawerButtons(),
+          ),
+          color: Color(0xFF620000),
           ),
         ),
-        drawer: Drawer(),
       ),
     );
   }
@@ -36,11 +41,75 @@ class _MenuState extends State<Menu>{
     return rp;
   }
   void _getName(String cookie,String url) async{
+    print("getting name");
     Future<Response> future = get(url,headers: {"cookie":cookie});
     future.then((Response response)=>{
-      setState(()=>{
-        _name = Utf8Decoder().convert(response.bodyBytes).substring(Utf8Decoder().convert(response.bodyBytes).indexOf("Üdv"),Utf8Decoder().convert(response.bodyBytes).indexOf("!",Utf8Decoder().convert(response.bodyBytes).indexOf("Üdv"))+1)
+      _appBarKey.currentState.setState(()=>{
+        _title[0] = Utf8Decoder().convert(response.bodyBytes).substring(Utf8Decoder().convert(response.bodyBytes).indexOf("Üdv"),Utf8Decoder().convert(response.bodyBytes).indexOf("!",Utf8Decoder().convert(response.bodyBytes).indexOf("Üdv"))+1)
       })
     });
   }
+}
+class DrawerButton extends StatelessWidget{
+  final int numOf;
+  const DrawerButton({@required this.numOf});
+  @override
+  Widget build(BuildContext context){
+    return OutlineButton(
+      child: DrawerText(numOf!=0?_title[numOf]:"Főoldal"),
+      borderSide: BorderSide(color: activePage==numOf?Colors.white:Colors.transparent,),
+      onPressed: (){activePage = numOf;Navigator.pop(context);_appBarKey.currentState.refresh();},
+    );
+  }
+}
+class DrawerText extends StatelessWidget{
+  final String text;
+  DrawerText(this.text);
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 22.0
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+class TitleText extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Text(
+      _title[activePage],
+      style: TextStyle(
+        color: Color(0xFFFFFFFF),
+        fontSize: 25.0
+      )
+    );
+  }
+}
+class PermaAppBar extends StatefulWidget implements PreferredSizeWidget{
+  PermaAppBar({Key key}):super(key:key);
+  @override
+  State<StatefulWidget> createState() {return _PermaAppBarState();}
+  @override
+  Size get preferredSize => Size.fromHeight(60.0);
+}
+class _PermaAppBarState extends State<PermaAppBar>{
+  void refresh()=>setState((){});
+  @override
+  Widget build(BuildContext context){
+    return AppBar(
+      backgroundColor: Color(0xFF620000),
+      title: TitleText(),
+    );
+  }
+}
+List<Widget> createDrawerButtons(){
+  List<Widget> list = new List(_title.length);
+  for (var i = 0; i < _title.length; i++) {
+    list[i] = DrawerButton(numOf:i);
+  }
+  return list;
 }
