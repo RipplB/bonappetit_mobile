f82a4a61e7cf692183a66023f4e9ded9f1dcbc4f
Balazs Rippl <balrip.play@gmail.com>
2019-11-25 08:48:00 +0100

    Unnecesery method removed

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,11 +1,2 @@
-import 'package:http/http.dart';
-
 int activePage = 0;
 String cookie;
-
-Response _getResponse(String cookie,String url){
-  Future<Response> future = get(url,headers: {"cookie":cookie});
-  Response rp;
-  future.then((Response response)=>{rp = response});
-  return rp;
-}
